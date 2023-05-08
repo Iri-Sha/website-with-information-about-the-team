92fb83dab4abd58954cad20673c37b3c67d6d47e
Iri-Sha <a72999555@gmail.com>
2023-05-08 09:27:20 +0500

    update settigs2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js",
-        publicPath: "/"
+        publicPath: ""
     },
     devServer: {
         static: path.resolve(__dirname, './dist'),
